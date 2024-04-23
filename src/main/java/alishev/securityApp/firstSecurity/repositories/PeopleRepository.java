52d02a97a78677dcbc5f2feb544622395aca5af1
ArsalanMark <a.markakov@gmail.com>
2024-04-23 23:24:25 +0800

    Initial commit

---
+++ src/main/java/alishev/securityApp/firstSecurity/repositories/PeopleRepository.java
@@ -0,0 +1,12 @@
+package alishev.securityApp.firstSecurity.repositories;
+
+import alishev.securityApp.firstSecurity.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByUsername(String username);
+}
---
+++ src/main/java/alishev/securityApp/firstSecurity/services/PersonDetailsService.java
@@ -0,0 +1,30 @@
+package alishev.securityApp.firstSecurity.services;
+
+import alishev.securityApp.firstSecurity.models.Person;
+import alishev.securityApp.firstSecurity.repositories.PeopleRepository;
+import alishev.securityApp.firstSecurity.security.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+    private PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<Person> person = peopleRepository.findByUsername(s);
+        if (person.isEmpty())
+            throw new UsernameNotFoundException("User not found");
+        return new PersonDetails(person.get());
+    }
+}
